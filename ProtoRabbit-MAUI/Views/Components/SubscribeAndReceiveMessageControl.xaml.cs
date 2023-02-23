69c4c9379460e3be8757f11913ba59a54b0bae12
Marius Muntean <marius.muntean@live.com>
2023-02-23 23:25:06 +0100

    Extracted a SubscribeAndReceiveMessageControl

---
+++ ProtoRabbit-MAUI/Views/Components/SubscribeAndReceiveMessageControl.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace ProtoRabbit.Views.Components;
+
+public partial class SubscribeAndReceiveMessageControl : ContentView
+{
+    public SubscribeAndReceiveMessageControl()
+    {
+        InitializeComponent();
+    }
+
+    public static readonly BindableProperty SubscriptionsProperty = BindableProperty.Create(
+        nameof(Subscriptions),
+        typeof(ObservableCollection<Services.Subscription>),
+        typeof(SubscriptionsManager)
+        );
+
+    public ObservableCollection<SubscriptionsManager> Subscriptions
+    {
+        get => (ObservableCollection<SubscriptionsManager>)GetValue(SubscriptionsProperty);
+        set => SetValue(SubscriptionsProperty, value);
+    }
+
+
+    public static readonly BindableProperty CurrentSubscriptionProperty = BindableProperty.Create(
+    nameof(CurrentSubscription),
+    typeof(Services.Subscription),
+    typeof(SubscriptionsManager)
+    );
+
+    public Services.Subscription CurrentSubscription
+    {
+        get => (Services.Subscription)GetValue(CurrentSubscriptionProperty);
+        set => SetValue(CurrentSubscriptionProperty, value);
+    }
+
+    void SubscriptionChanged(System.Object sender, Microsoft.Maui.Controls.SelectionChangedEventArgs e)
+    {
+        CurrentSubscription = (Services.Subscription)e.CurrentSelection.FirstOrDefault();
+        SelectedSubscriptionChangedCommand?.Execute(null);
+    }
+
+
+    public static readonly BindableProperty OpenSubscriptionEditorCommandProperty = BindableProperty.Create(
+        nameof(OpenSubscriptionEditorCommand),
+        typeof(ICommand),
+        typeof(SubscriptionsManager)
+        );
+
+    public ICommand OpenSubscriptionEditorCommand
+    {
+        get => (ICommand)GetValue(OpenSubscriptionEditorCommandProperty);
+        set => SetValue(OpenSubscriptionEditorCommandProperty, value);
+    }
+
+    void OpenSubscriptionEditorClicked(System.Object sender, System.EventArgs e)
+    {
+        OpenSubscriptionEditorCommand?.Execute(null);
+    }
+
+
+    public static readonly BindableProperty SelectedSubscriptionChangedCommandProperty = BindableProperty.Create(
+        nameof(SelectedSubscriptionChangedCommand),
+        typeof(ICommand),
+        typeof(SubscriptionsManager)
+        );
+
+    public ICommand SelectedSubscriptionChangedCommand
+    {
+        get => (ICommand)GetValue(SelectedSubscriptionChangedCommandProperty);
+        set => SetValue(SelectedSubscriptionChangedCommandProperty, value);
+    }
+
+
+    public static readonly BindableProperty StopSubscriptionCommandProperty = BindableProperty.Create(
+    nameof(StopSubscriptionCommand),
+    typeof(ICommand),
+    typeof(SubscriptionsManager)
+    );
+
+    public ICommand StopSubscriptionCommand
+    {
+        get => (ICommand)GetValue(StopSubscriptionCommandProperty);
+        set => SetValue(StopSubscriptionCommandProperty, value);
+    }
+
+    void RemoveSubscriptionClicked(System.Object sender, System.EventArgs e)
+    {
+        if (sender is MenuFlyoutItem menuFlyoutItem && menuFlyoutItem.CommandParameter is Services.Subscription sub)
+        {
+            StopSubscriptionCommand?.Execute(sub);
+        }
+    }
+}
