01b467bd2d076d2883ec9cdb4b812f752bc7ece7
Marius Muntean <marius.muntean@live.com>
2023-02-28 22:22:47 +0100

    Disabled node integration and enabled context isolation

--- ProtoRabbit-Electron/src/App.tsx
+++ ProtoRabbit-Electron/src/App.tsx
@@ -2,7 +2,6 @@ import nodeLogo from "./assets/node.svg"
 import { useState } from 'react'
 import './App.scss'
 
-console.log('[App.tsx]', `Hello world from Electron ${process.versions.electron}!`)
 
 function App() {
   const [count, setCount] = useState(0)
