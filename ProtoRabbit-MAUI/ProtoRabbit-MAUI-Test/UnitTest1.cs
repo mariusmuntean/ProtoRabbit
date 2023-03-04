801ddc0c12bf430807b135021910aab0bc6da785
Marius Muntean <marius.muntean@live.com>
2023-03-04 16:09:43 +0100

    Added the test project

---
+++ ProtoRabbit-MAUI/ProtoRabbit-MAUI-Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ProtoRabbit_MAUI_Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
