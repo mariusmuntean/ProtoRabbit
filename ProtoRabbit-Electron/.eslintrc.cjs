3685c334d7b94fd05b4c07640320b3cf0956c3a7
Marius Muntean <marius.muntean@live.com>
2023-03-05 18:03:29 +0100

    Installed eslint-plugin-react-hooks

--- ProtoRabbit-Electron/.eslintrc.cjs
+++ ProtoRabbit-Electron/.eslintrc.cjs
@@ -14,7 +14,7 @@ module.exports = {
     sourceType: 'module',
     ecmaVersion: 2021
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'react-hooks'],
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
@@ -30,7 +30,9 @@ module.exports = {
     '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/no-non-null-assertion': 'off',
-    '@typescript-eslint/no-var-requires': 'off'
+    '@typescript-eslint/no-var-requires': 'off',
+    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+    'react-hooks/exhaustive-deps': 'warn' // Checks effect dependencies
   },
   overrides: [
     {
