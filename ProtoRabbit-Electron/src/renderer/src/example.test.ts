9d4b849303a671c23cf7ce22c12402f307986461
Marius Muntean <marius.muntean@live.com>
2023-04-08 17:42:39 +0200

    Simplified the Playwright test

--- ProtoRabbit-Electron/src/renderer/src/example.test.ts
+++ ProtoRabbit-Electron/src/renderer/src/example.test.ts
@@ -1,26 +1,15 @@
 import { _electron as electron } from 'playwright'
-import { test, expect } from '@playwright/test'
+import { test } from '@playwright/test'
 
 test('launch app', async ({ page }, i) => {
   const electronApp = await electron.launch({ args: ['./out/main/index.js'] })
-  const isPackaged = await electronApp.evaluate(async ({ app }) => {
-    // This runs in Electron's main process, parameter here is always
-    // the result of the require('electron') in the main app script.
-    return app.isPackaged
-  })
-
-  expect(isPackaged).toBe(false)
-
-  // Wait for the first BrowserWindow to open
-  // and return its Page object
   const window = await electronApp.firstWindow()
 
   const connectBtn = window.getByRole('button', { name: 'Connect' })
   await connectBtn.click()
 
-  // const disconnectBtn = window.getByRole('button', { name: 'Disconnect' })
-  // await disconnectBtn.click()
+  const disconnectBtn = window.getByRole('button', { name: 'Disconnect' })
+  await disconnectBtn.click()
 
-  // close app
-  await electronApp.close()
+  await window.close()
 })
