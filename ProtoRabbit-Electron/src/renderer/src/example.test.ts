4ab4bd5d589fdc89ec3374c559a1ef2437f877e7
Marius Muntean <marius.muntean@live.com>
2023-04-09 10:54:08 +0200

    Testing

--- ProtoRabbit-Electron/src/renderer/src/example.test.ts
+++ ProtoRabbit-Electron/src/renderer/src/example.test.ts
@@ -2,7 +2,7 @@ import { _electron as electron } from 'playwright'
 import { test } from '@playwright/test'
 
 test('launch app', async ({ page }, i) => {
-  const electronApp = await electron.launch({ args: ['./out/main/index.js'] })
+  const electronApp = await electron.launch({ args: ['./../../../out/main/index.js'] })
   const window = await electronApp.firstWindow()
 
   const connectBtn = window.getByRole('button', { name: 'Connect' })
